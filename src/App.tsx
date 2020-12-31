e2c34277ad93536a57b73bc33dac411d00f2b0d3
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2020-12-31 07:36:18 +0700

    build-antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Button, Layout } from "antd";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const { Header, Content, Footer } = Layout;
+  return (
+    <div>
+      <Layout>
+        <Header style={{ display: "flex", justifyContent: "space-around" }}>
+          <span>Recipe GG</span>
+          <Button type="primary">Add Recipe</Button>
+        </Header>
+        <Content></Content>
+        <Footer></Footer>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
