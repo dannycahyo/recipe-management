685b7c72b7b89455c91b84100de6400a85fd94fe
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-05-26 19:39:52 +0700

    handle loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense, lazy } from "react";
+import { Grid, GridItem, Heading, Image } from "@chakra-ui/react";
 import { Switch, Route } from "react-router-dom";
+import loadingImg from "./assets/undraw_season_change_f99v.svg";
 
 const LandingPage = lazy(() => import("./containers/LandingPage"));
 const MyRecipe = lazy(() => import("../src/containers/MyRecipe"));
@@ -7,7 +9,23 @@ const MyRecipe = lazy(() => import("../src/containers/MyRecipe"));
 export default function App() {
   return (
     <div>
-      <Suspense fallback={<h1>Loading, Please wait</h1>}>
+      <Suspense
+        fallback={
+          <Grid justifyContent="center" alignItems="center">
+            <GridItem colStart={2} colEnd={5} p="12">
+              <Heading
+                size="xl"
+                textColor="pink.300"
+                textAlign="center"
+                mb="12"
+              >
+                Loading, Please Wait For A While
+              </Heading>
+              <Image alt="Not Found" src={loadingImg} h="370" w="475" />
+            </GridItem>
+          </Grid>
+        }
+      >
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/myrecipes" component={MyRecipe} />
